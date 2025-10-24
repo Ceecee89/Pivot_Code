194a0dc130bf721b0d61d225e0c5c07ab43c001c
Cherylirene <cherylirene89@gmail.com>
2025-10-23 20:29:42 -0500

    toggleCheck function added with if else statement

--- task-app/src/components/TaskList.jsx
+++ task-app/src/components/TaskList.jsx
@@ -1,8 +1,9 @@
-//array of objects
+import { useState } from "react"
+//array of objects (arrays in the "[" list things, objects group stuff together)
 const TODO_LIST = [
-    { title: "Get Milk", dueDate: "10/31/2025", done: true },
+    { title: "Get Milk", dueDate: "10/31/2025" },
     { title: "Pay Bills", dueDate: "11/05/2025" },
-    { title: "Christmas Shopping", dueDate: "12/03/2025", done: true },
+    { title: "Christmas Shopping", dueDate: "12/03/2025" },
     { title: "Create Budget List", dueDate: "01/01/2026" }
     // make 3 more
 ]
@@ -10,20 +11,50 @@ console.log(TODO_LIST[0])
 
 // individual items, props is for passing down
 function TaskItem(props) {
+    //track if the checkbox was clicked
+
+    //let isDone = true;
+    //if we want a variable to update the site, we use state!
+    // left side is the name of variable | right side is the name of function
+    const [isDone, setIsDone] = useState(true); //true creates the slash throught the items, false leaves the list items without the slash and unable to check the checkboxes
+    console.log(props.title, isDone);
+
+    function toggleCheck() {//only here is connected to the click
+        //make isDone equal false
+        //isDone = false;
+        setIsDone(false); //isDone = false;
+        console.log(isDone);
+        console.log(props.title, isDone)
+        if (isDone == true) {
+            setIsDone(false)
+        }
+        else {
+            setIsDone(true);
+        }
+
+
+    }
+
     return (
         // ? is the same as if,  : is the same as else
         <div className="task">
             <h3 className={
-                props.done == true ? "done" : ""}>{props.title}</h3>
+                isDone == true ? "done" : ""}>{props.title}</h3>
             <p>Due: {props.dueDate}</p>
-            <input type='checkbox' />
+            <input
+                type='checkbox'
+                onChange={toggleCheck}
+                checked={isDone} />
         </div>
 
     )
 }
+//when one item is changed they all change (example when one is checked in the TaskItem fucnction to either true or false, they all become checked or unchecked)
+//components are their own thing example: each task item is it's own component)
 // list of items
 // with props add the curly brackets and type {props. whatever is in your return 
-
+// always console.log it helps us test the code
+//return part is html
 function TaskList() {
     return (
         <section>
